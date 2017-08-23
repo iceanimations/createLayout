e956cd176ce224f1d2c2891c07e8b52c2635157a
Talha Ahmed <talha.ahmed@gmail.com>
2017-08-23 18:39:00 +0500

    change RS Proxies to bounding box before saving layout file

--- src/utilities.py
+++ src/utilities.py
@@ -12,22 +12,45 @@ projectKey = 'createLayoutProjectKey'
 episodeKey = 'createLayoutEpisodeKey'
 sequenceKey = 'createLayoutSequenceKey'
 
+
+class RSProxyDisplayMode:
+    BoundingBox = BB = 0
+    PreviewMesh = PM = 1
+    LinkedMesh = LM = 2
+
+
+def switchRSProxyDisplayMode(val=RSProxyDisplayMode.BB):
+    for node in pc.ls(type='RedshiftProxyMesh'):
+        if node.displayMode.get() != val:
+            node.displayMode.set(val)
+
+
 def createCacheNamesOnGeoSets():
-    geosets = [gs for gs in pc.ls(exactType=pc.nt.ObjectSet)
-               if gs.name().lower().endswith('_geo_set')]
-    counts = Counter([imaya.getNiceName(geoset.name()).replace('_geo_set', '') for geoset in geosets])
-    counts = {key: list(reversed(range(1, val + 1))) for key, val in counts.items()}
+    geosets = [
+        gs for gs in pc.ls(exactType=pc.nt.ObjectSet)
+        if gs.name().lower().endswith('_geo_set')
+    ]
+    counts = Counter([
+        imaya.getNiceName(geoset.name()).replace('_geo_set', '')
+        for geoset in geosets
+    ])
+    counts = {
+        key: list(reversed(range(1, val + 1)))
+        for key, val in counts.items()
+    }
     for geoset in geosets:
         niceName = imaya.getNiceName(geoset.name()).replace('_geo_set', '')
         if not geoset.hasAttr('cacheName'):
             pc.addAttr(geoset, sn='cname', ln='cacheName', dt='string', h=True)
-        niceName += '_%s'%str(counts[niceName].pop()).zfill(2)
+        niceName += '_%s' % str(counts[niceName].pop()).zfill(2)
         geoset.cacheName.set(niceName)
-        
+
+
 def createProjectContext(project, episode, sequence):
     imaya.addFileInfo(projectKey, project)
     imaya.addFileInfo(episodeKey, episode)
     imaya.addFileInfo(sequenceKey, sequence)
-    
+
+
 def saveScene():
     cmds.file(save=True, f=True)
